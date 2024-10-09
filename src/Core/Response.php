b31b1cb227914e914364046880832a9e1b7cd2ea
brunocriacoes <br.rafael@outlook.com>
2024-10-09 13:42:40 -0300

    fix

--- src/Core/Response.php
+++ src/Core/Response.php
@@ -9,8 +9,21 @@ class Response
         http_response_code($code);
     }
 
-    public function body(array $payload): void
+    public function body(array $payload)
     {
         echo json_encode($payload, JSON_UNESCAPED_UNICODE);
+        return $this;
+    }
+
+    public function json(int $code, array $payload): void
+    {
+        http_response_code($code);
+        echo json_encode($payload, JSON_UNESCAPED_UNICODE);
+    }
+    
+    public function error(int $code, string $message): void
+    {
+        http_response_code($code);
+        echo $message;
     }
 }
